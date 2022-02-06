f3a9f16938c37c2c3dc9f119878657342bb9ab93
rqthomas <rqthomas@vt.edu>
2022-02-06 20:31:39 +0000

    round 2 updates

--- cron_automation.R
+++ cron_automation.R
@@ -3,14 +3,14 @@ library(cronR)
 home_dir <- "/home/rstudio/Documents/scripts"
 log_dir <- "/efi_neon_challenge/log/cron"
 
-terrestrial_repo <- "neon4cast-terrestrial"
+aquatics_repo <- "neon4cast-aquatics"
 
 #Go to healthchecks.io. Create a project.  Add a check. Copy the url and add here.  
-health_checks_url <- "https://hc-ping.com/bbe3ddc7-4020-4c53-bb13-08580d765e32"
+health_checks_url <- "https://hc-ping.com/a848914e-9abf-45e4-bcf3-27f570cc3623"
 
-cmd <- cronR::cron_rscript(rscript = file.path(home_dir, terrestrial_repo,"run-terrestrial-null-models.R"),
-                           rscript_log = file.path(log_dir, "terrestrial.log"),
+cmd <- cronR::cron_rscript(rscript = file.path(home_dir, aquatics_repo,"03_generate_null_forecast_aquatics.R"),
+                           rscript_log = file.path(log_dir, "aquatics-climatology.log"),
                            log_append = FALSE,
-                           workdir = file.path(home_dir, terrestrial_repo),
+                           workdir = file.path(home_dir, aquatics_repo),
                            trailing_arg = paste0("curl -fsS -m 10 --retry 5 -o /dev/null ", ))
-cronR::cron_add(command = cmd, frequency = 'daily', at = "11AM", id = 'terrestrial-null-models')
+cronR::cron_add(command = cmd, frequency = 'daily', at = "11AM", id = 'aquatics-null-models')
