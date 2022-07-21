f9acaba70e65239d73818485f7c4ecc5f7f892b7
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2022-07-21 20:41:10 +0000

    keep only subset of variables from avro

--- 02_reading_in_provisional_avros.R
+++ 02_reading_in_provisional_avros.R
@@ -11,5 +11,23 @@ Sys.setenv(TZ = 'UTC')
 # spark_install(version = '2.4')
 sc <- sparklyr::spark_connect(master = "local")
 
+
+variables_keep <- c('siteName', 'startDate', 'sensorDepth',
+                    'dissolvedOxygen','dissolvedOxygenExpUncert','dissolvedOxygenFinalQF', 
+                    'chlorophyll', 'chlorophyllExpUncert','chlorophyllFinalQF')
+
+columns_keep <- c('siteName', 'termName', 'startDate', 'Value', 'sensorDepth')
+
+# read in avro and filter based on variables
 wq_avro <- spark_read_avro(sc, name = 'name', path = file_path_sc) %>%
-  as_tibble()
+  as_tibble() %>%
+  filter(termName %in% variables_keep) %>%
+  # for streams want to omit the downstream measurement (102) and retain upstream (101)
+  # rivers and lakes horizontal index is 103
+  filter(horizontalIndex %in% c('101', '103')) %>%
+  select(siteName, termName, startDate, 
+         doubleValue, intValue) %>%
+  # combine the value fields to one
+  mutate(Value = ifelse(is.na(doubleValue), 
+                        intValue, doubleValue)) %>%
+  select(any_of(columns_keep))
